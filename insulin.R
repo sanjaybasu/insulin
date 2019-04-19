ed86580549112a289b1717447dabbe99f859ee3b
Sanjay Basu <sanjayb493@gmail.com>
2019-04-19 16:07:29 -0700

    Update insulin.R

--- insulin.R
+++ insulin.R
@@ -2,17 +2,20 @@ rm(list=ls())
 library(tidyverse)
 library(survival)
 library(readr)
-library(broom)
+library(plm)
 library(knitr)
+library(broom)
+library(riskRegression)
 
 iseq <- read_csv("~/Transform_15_output.csv")
-iev <- read_csv("~/Transform_18_output.csv", 
+iev <- read_csv("~/Transform_26_output.csv", 
                 col_types = cols(zip = col_double()))
 
-temp <- subset(iev, select=c(zip:event)) # baseline data
+temp <- subset(iev, select=c(type, zip:event)) # baseline data
 temp = temp %>%
   group_by(Patid) %>%
-  summarise(zip = max(zip),
+  summarise(type = max(type),
+            zip = max(zip),
             sex = max(sex),
             age = min(age),
             time = max(time),
@@ -23,17 +26,116 @@ temp2 = temp %>%
   select(Patid)
 temp3 = iseq %>%
   inner_join(temp2, by="Patid") %>%
-  mutate(dolperunits = dolperunits*1.07*(Std_Cost_Yr==2013)+
-           dolperunits*1.08*(Std_Cost_Yr!=2013),
+  mutate(dolper1000units = (oopc/unitsfixed)*1000,
+         dolper1000units = dolper1000units*1.07*(Std_Cost_Yr==2013)+
+           dolper1000units*1.08*(Std_Cost_Yr!=2013),
          year = lubridate::year(Fill_Dt), 
          month = lubridate::month(Fill_Dt)) %>%
-  mutate_at(scale, .vars = "dolperunits")
+  mutate_at(scale, .vars = "dolper1000units")
 
 iev2 <- tmerge(temp, temp, id=Patid, endpt = event(time, event))
-iev2 <- tmerge(iev2, temp3, id=Patid, dolperunits = tdc(time, dolperunits),
-               month = tdc(time, month),
+iev2 <- tmerge(iev2, temp3, id=Patid, dolper1000units = tdc(time, dolper1000units),
+               year = tdc(time, year))
+fit1_unadj = coxph(Surv(tstart, tstop, endpt==1) ~ dolper1000units+cluster(Patid), data=iev2, x = T)
+summary(fit1_unadj)
+
+fit1 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iev2, x = T)
+summary(fit1)
+
+fit1_s1 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iev2, subset=(age<18))
+summary(fit1_s1)
+
+fit1_s2 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iev2, subset=((age>=18) & (age<65)))
+summary(fit1_s2)
+
+fit1_s3 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iev2, subset=(age>=65))
+summary(fit1_s3)
+
+fit1_s4 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iev2, subset=(type==1))
+summary(fit1_s4)
+
+fit1_s5 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iev2, subset=(type==2))
+summary(fit1_s5)
+
+
+
+
+iem <- read_csv("~/Transform_32_output.csv", 
+                col_types = cols(zip = col_double()))
+
+temp <- subset(iem, select=c(type, zip:event)) # baseline data
+temp = temp %>%
+  group_by(Patid) %>%
+  summarise(type = max(type),
+            zip = max(zip),
+            sex = max(sex),
+            age = min(age),
+            time = max(time),
+            event = max(event),
+            timeenr = max(timeenr)) 
+
+temp2 = temp %>%
+  select(Patid)
+temp3 = iseq %>%
+  inner_join(temp2, by="Patid") %>%
+  mutate(dolper1000units = (oopc/unitsfixed)*1000,
+         dolper1000units = dolper1000units*1.07*(Std_Cost_Yr==2013)+
+           dolper1000units*1.08*(Std_Cost_Yr!=2013),
+         year = lubridate::year(Fill_Dt), 
+         month = lubridate::month(Fill_Dt)) %>%
+  mutate_at(scale, .vars = "dolper1000units")
+
+iem2 <- tmerge(temp, temp, id=Patid, endpt = event(time, event))
+iem2 <- tmerge(iem2, temp3, id=Patid, dolper1000units = tdc(time, dolper1000units),
                year = tdc(time, year))
 
-fit1 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+dolperunits+cluster(Patid), data=iev2)
-summary(fit1)
+fit2_unadj = coxph(Surv(tstart, tstop, endpt==1) ~ dolper1000units+cluster(Patid), data=iem2)
+summary(fit2_unadj)
+
+fit2 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iem2)
+summary(fit2)
+
+fit2_s1 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iem2, subset=(age<18))
+summary(fit2_s1)
+
+fit2_s2 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iem2, subset=((age>=18) & (age<65)))
+summary(fit2_s2)
+
+fit2_s3 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iem2, subset=(age>=65))
+summary(fit2_s3)
+
+fit2_s4 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iem2, subset=(type==1))
+summary(fit2_s4)
+
+fit2_s5 = coxph(Surv(tstart, tstop, endpt==1) ~ age+sex+zip+timeenr+year+dolper1000units+cluster(Patid), data=iem2, subset=(type==2))
+summary(fit2_s5)
+
+
+
+hba1c <- read_csv("~/Transform_34_output.csv")
+data1c = hba1c %>%
+  full_join(temp, by="Patid") %>%
+  inner_join(iseq, by="Patid") %>%
+  mutate(a1c = Rslt_Nbr*(Rslt_Nbr>0),
+         days = lubridate::day(Fst_Dt)-lubridate::day(Fill_Dt),
+         dolper1000units = (oopc/unitsfixed)*1000,
+         dolper1000units = dolper1000units*1.07*(Std_Cost_Yr==2013)+
+           dolper1000units*1.08*(Std_Cost_Yr!=2013),
+         year = lubridate::year(Fst_Dt)) %>%
+  filter(days>0) %>%
+  mutate_at(scale, .vars = "dolper1000units") %>%
+  group_by(Patid, Fst_Dt) %>%
+  slice(which.min(days)) %>%
+  ungroup() %>%
+  unique()
+
+summary(data1c$a1c)
+
+panel <- pdata.frame(data1c, index=c("Patid", "Fst_Dt"))
+kable(tidy(plm(a1c~dolper1000units-1, data=panel, model="fd")), digits=3)
+kable(tidy(plm(a1c~dolper1000units-1, data=panel, model="fd", subset=(age.x<18))), digits=3)
+kable(tidy(plm(a1c~dolper1000units-1, data=panel, model="fd", subset=((age.x>=18) & (age.x<65)))), digits=3)
+kable(tidy(plm(a1c~dolper1000units-1, data=panel, model="fd", subset=(age.x>=65))), digits=3)
+kable(tidy(plm(a1c~dolper1000units-1, data=panel, model="fd", subset=(type==1))), digits=3)
+kable(tidy(plm(a1c~dolper1000units-1, data=panel, model="fd", subset=(type==2))), digits=3)
 
